c0a7f95d80f88e412bcbab71913b58546444c987
Olina Savčuková <olinkasavcuk@gmail.com>
2025-11-12 16:07:58 +0100

    Fix date formatting in projectApi.js

--- server/Project/index.js
+++ server/Project/index.js
@@ -8,7 +8,7 @@ const ports = process.env.PORT || 3000;
 
 // Enable CORS for frontend communication
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Vite default port
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
