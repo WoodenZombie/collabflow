bdb76a2aa5599053ebcf0a21a66c93fb14517601
jansvobodaa <svoboda.assasins@gmail.com>
2025-11-24 21:33:48 +0100

    created appointment validation

--- server/Appointment/validation/appointment.js
+++ server/Appointment/validation/appointment.js
@@ -8,6 +8,23 @@ exports.appointmentValidation = [
     .isLength({ max: 255 })
     .withMessage('Title must not exceed 255 characters'),
 
+  body('location')
+    .optional()
+    .isLength({ max: 255 })
+    .withMessage('Location must not exceed 255 characters'),
+
+  body('start_time')
+    .notEmpty()
+    .withMessage('Start time is required')
+    .isISO8601()
+    .toDate()
+    .withMessage('Start time must be a valid date'),
+
+  body('task_id')
+    .optional()
+    .isInt()
+    .withMessage('Task ID must be an integer'),
+
 //We use validation in router. And when it comes to to an error it sents error 400
     (req, res, next)=>{
         const result = validationResult(req);
